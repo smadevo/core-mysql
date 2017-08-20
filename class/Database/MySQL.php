bc129980cd89569f37045017c010610fdba64cff
Jens Scherbl <jens@scherbl.com>
2017-08-20 14:06:59 +0200

    Refactored database component

--- class/Database/MySQL.php
+++ class/Database/MySQL.php
@@ -1,36 +1,47 @@
 <?php
 namespace App\Database;
 
+use PDO;
+
 /**
+ * Configures a MySQL connection.
+ *
  * @inheritDoc
  */
-final class MySQL extends Base
+final class MySQL
 {
     /**
-     * Constructor.
+     * Configures the connection.
      *
      * @param string $host
      * @param string $port
      * @param string $database
      * @param string $username
      * @param string $password
+     *
+     * @return Base
+     *
+     * @uses Base
+     * @uses PDO
      */
-    public function __construct(
+    public static function create(
         string $host,
         string $port,
         string $database,
         string $username,
         string $password
-    ) {
-        parent::__construct(
+    ): Base {
+        $pdo = new PDO(
             "mysql:host={$host};port={$port};dbname={$database}",
             $username,
             $password
         );
-        $this->exec('SET sql_mode                 = ANSI'   );
-        $this->exec('SET time_zone                = +00:00' );
-        $this->exec('SET character_set_client     = utf8mb4');
-        $this->exec('SET character_set_results    = utf8mb4');
-        $this->exec('SET character_set_connection = utf8mb4');
+        $pdo->exec('SET sql_mode                 = ANSI'   );
+        $pdo->exec('SET time_zone                = +00:00' );
+        $pdo->exec('SET character_set_client     = utf8mb4');
+        $pdo->exec('SET character_set_results    = utf8mb4');
+        $pdo->exec('SET character_set_connection = utf8mb4');
+
+        return new Base($pdo);
     }
 }
